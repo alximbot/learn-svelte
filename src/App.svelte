b54eee91575ee53229a358dbebc35d3cd67d995f
John Lee <5065573+alximbot@users.noreply.github.com>
2021-05-11 19:33:12 +1200

    1. Add Title

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
-	export let name: string;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Svelte TODO List</h1>
+	<p>Let's build this!</p>
 </main>
 
 <style>
