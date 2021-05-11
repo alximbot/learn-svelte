6e7b7dc974cca1fe92b1c80d57d96d3cb530910e
John Lee <5065573+alximbot@users.noreply.github.com>
2021-05-11 20:23:56 +1200

    4. Input 2 way binding example

--- src/List.svelte
+++ src/List.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
   const list: string[] = ["Eat breakfast", "Work out", "Meditate"];
+  let newItem: string = "";
 </script>
+
+<input bind:value={newItem} />
 
 {#each list as item}
   <h2>{item}</h2>
 {/each}
+
+<h2>{newItem}</h2>
