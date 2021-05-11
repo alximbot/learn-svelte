caace9008f4a67f9d1ac77b45f3c0ffaebb153ef
John Lee <5065573+alximbot@users.noreply.github.com>
2021-05-11 19:43:07 +1200

    2. Hard coded list

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,26 @@
 <script lang="ts">
+  const list: string[] = ["Eat breakfast", "Work out", "Meditate"];
 </script>
 
 <main>
-	<h1>Svelte TODO List</h1>
-	<p>Let's build this!</p>
+  <h1>Svelte TODO List</h1>
+  {#each list as item}
+    <h2>{item}</h2>
+  {/each}
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 500px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 </style>
