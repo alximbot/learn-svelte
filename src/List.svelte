115e3fef06854b4dd30bad5fa18b697394fd9cf6
John Lee <5065573+alximbot@users.noreply.github.com>
2021-05-11 21:22:44 +1200

    6. Add new input HTML

--- src/List.svelte
+++ src/List.svelte
@@ -2,6 +2,11 @@
   const list: string[] = ["Eat breakfast", "Work out", "Meditate"];
 </script>
 
+<div class="newInputContainer">
+  <input class="newInput" placeholder="" />
+  <button class="addButton">Add</button>
+</div>
+
 <div class="listInputs">
   {#each list as item}
     <input bind:value={item} />
@@ -15,14 +20,41 @@
     row-gap: 12px;
   }
 
-  input {
+  .listInputs input {
     color: #6c6c6c;
     font-size: 1.5em;
     border: none;
     outline: none;
   }
 
-  input:focus {
+  .listInputs :focus {
     color: #1a1a1a;
+  }
+
+  .newInputContainer {
+    display: grid;
+    grid-template-columns: auto 80px;
+    margin-bottom: 40px;
+  }
+
+  .newInputContainer > * {
+    height: 100%;
+  }
+
+  input.newInput {
+    color: #1a1a1a;
+    font-size: 1.5em;
+    border: 1px solid rgba(coral, 0.3);
+    border-radius: 2px;
+    margin-right: 2px;
+  }
+
+  input.newInput:focus {
+    border: 1px solid coral;
+    outline: none;
+  }
+
+  .addButton {
+    cursor: pointer;
   }
 </style>
